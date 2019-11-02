3734bbd810699201d7e60686e9d891850de932b3
Pascual10 <56838611+Pascual10@users.noreply.github.com>
2019-11-02 18:21:11 +0100

    Add files via upload

--- src/Domain/LZSS.java
+++ src/Domain/LZSS.java
@@ -1,62 +1,158 @@
 package Domain;
 
-public class LZSS extends Algorithm{
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
-    public void comprimir(String InFile, String NewName, String NewPath){
+public class LZSS extends Algorithm {
 
-        FileInputStream FIn = new FileInputStream(InFile);
-        FileOutputStream FOut = new FileOutputStream(NewPath + NewName + ".lzss");
-        bytebuffer Ventana = bytebuffer.allocate(4092);
+    private FileReader FIn;
+    private FileWriter FOut;
+    private StringBuilder Ventana;
+    private short nextChar;
 
-        byte nextByte;
-        String ActualMatch = "";
-        int MIndex = 0, tempIndex = 0, MidaMatch = 0;
 
-        while(FIn.read(nextByte) != -1){
-            //Busquem en la finestra
-            tempIndex = Ventana.indexOf(ActualMatch + (char)nextByte);
+    public LZSS() {
+
+    }
+
+
+    public void comprimir(String InFile, String NewName, String NewPath) throws IOException {
+
+        FIn = new FileReader(InFile);
+        FOut = new FileWriter(NewPath + "\\" + NewName + ".lzss");
+        Ventana = new StringBuilder();
+        StringBuilder ActualMatch = new StringBuilder();
+
+        int MIndex = 0;
+        int tempIndex;
+        int MidaOrig = 0;
+        int MidaComp = 0;
+        int MidaMatch = 0;
+
+        while((nextChar =  (short) FIn.read()) != -1){
+            ++MidaOrig;
+            ActualMatch.append((char)nextChar);
+            tempIndex = Ventana.indexOf(ActualMatch.toString());
 
             if(tempIndex != -1){
-                ActualMatch += (char)nextByte;
                 MIndex = tempIndex;
-                MidaMatch += 1;
+                ++MidaMatch;
             }
-            else{  //No hi ha match mes llarg
-                if(MidaMatch > 3){  //Si la mida es mes gran que 3, codifica el match
-                    MIndex -= MidaMatch; //Tornem a la posicio inicial del match
-                    byte[] Codi = byte[3];
-                    Codi[0] = (byte)1; //Flag a 1
-                    Codi[1] = (byte)(MIndex >> 4);
-                    byte Aux = (byte)(((MIndex & 0b1111) << 4) || (MidaMatch & 0b1111));
-                    Codi[2] = Aux;
-                    FOut.write(Codi);  //Escribim els 3 bytes codificats
+            else{
+                if (MidaMatch > 3) {  //Si la mida es mes gran que 3, codifica el match
+                    char[] Codi = new char[3];
+                    Codi[0] = 1; //Flag a 1
+                    Codi[1] = (char)MIndex;
+                    Codi[2] = (char)MidaMatch;
+                    FOut.write(Codi);  //Escribim els 3 chars codificats
+                    MidaComp += 3;
 
-                    while(Ventana.size() + MidaMatch > 4092){
-                        Ventana.delete(0);  //Eliminem de la finestra els MidaMatch primers elements
-                    }
-                    Ventana.put(ActualMatch); //Posem a la finestra els bytes del match
+                    Ventana.append(ActualMatch.substring(0, MidaMatch));
+                    while(Ventana.length() > 65536) Ventana.deleteCharAt(0);
 
-                    ActualMatch = "";
-                    MidaMatch = 0;
+                    while(ActualMatch.length() > 1) ActualMatch.deleteCharAt(0);
+                    MIndex = -1;
+                    MidaMatch = 1;
+
+                } else {  //Si la mida no es mes gran que 3, escriu flag a 0 i char sense codificar
+                    FOut.append((char)0);
+                    FOut.append(ActualMatch.charAt(0));
+                    MidaComp += 2;
+                    Ventana.append(ActualMatch.charAt(0)); //Posem a la finestra el primer char del match
+                    if (Ventana.length() > 65536)  Ventana.deleteCharAt(0);  //Eliminem de la finestra el primer element
+
+                    ActualMatch.deleteCharAt(0);
+                    if(ActualMatch.length() == 0) MidaMatch = 0;
+
                     MIndex = -1;
                 }
-                else{  //Si la mida no es mes gran que 3, escriu flag a 0 i byte sense codificar
-                    FOut.write((byte)0);
+            }
+        }
+        while(MidaMatch > 0){
+            MIndex = Ventana.indexOf(ActualMatch.toString());
 
-                    if(Ventana.size() >= 4092) Ventana.delete(0);  //Eliminem de la finestra el primer element
+            if(MIndex != -1){
+                if (MidaMatch > 3) {  //Si la mida es mes gran que 3, codifica el match
+                    char[] Codi = new char[3];
+                    Codi[0] = 1; //Flag a 1
+                    Codi[1] = (char)MIndex;
+                    Codi[2] = (char)MidaMatch;
+                    FOut.write(Codi);  //Escribim els 3 chars codificats
+                    MidaComp += 3;
 
-                    if(MidaMatch > 2){
-                        FOut.write(nextByte);  //Escribim el byte actual a FOut
-                        Ventana.put(nextByte); //Posem a la finestra el byte actual
-                    }
-                    else{
-                        FOut.write(ActualMatch[0]);
-                        Ventana.put(ActualMatch[0]); //Posem a la finestra el primer byte del match
-                    }
+                    Ventana.append(ActualMatch.substring(0, MidaMatch));
+                    while(Ventana.length() > 65536) Ventana.deleteCharAt(0);
 
-                    ActualMatch = "";
+                    while(ActualMatch.length() > 0) ActualMatch.deleteCharAt(0);
                     MidaMatch = 0;
-                    MIndex = -1;
+
+                } else {  //Si la mida no es mes gran que 3, escriu flag a 0 i char sense codificar
+                    FOut.append((char)0);
+                    FOut.append(ActualMatch.charAt(0));
+                    MidaComp += 2;
+
+                    Ventana.append(ActualMatch.charAt(0)); //Posem a la finestra el primer char del match
+                    if (Ventana.length() > 65536)  Ventana.deleteCharAt(0);  //Eliminem de la finestra el primer element
+
+                    ActualMatch.deleteCharAt(0);
+                    if(ActualMatch.length() == 0) MidaMatch = 0;
+                }
+            }
+            else{
+                FOut.append((char)0);
+                FOut.append(ActualMatch.charAt(0));
+                MidaComp += 2;
+
+                Ventana.append(ActualMatch.charAt(0)); //Posem a la finestra el primer char del match
+                if (Ventana.length() > 65536)  Ventana.deleteCharAt(0);  //Eliminem de la finestra el primer element
+
+                ActualMatch.deleteCharAt(0);
+                --MidaMatch;
+            }
+        }
+        //MAL
+        System.out.println("Mida del Fitxer Original: " + MidaOrig);
+        System.out.println("Mida del Fitxer Comprimit: " + MidaComp);
+        double OpMat = ((double)MidaComp/(double)MidaOrig)*100;
+        System.out.println("Percentatge de compressio: " + OpMat);
+        FIn.close();
+        FOut.close();
+    }
+
+
+    public void descomprimir(String InFile, String NewName, String NewPath) throws IOException {
+
+        FIn = new FileReader(InFile);
+        FOut = new FileWriter(NewPath + "\\" + NewName + ".txt");
+        Ventana = new StringBuilder();
+        int nextInt;
+
+        while((nextChar = (short)FIn.read()) != -1){
+
+            if(nextChar == 0){
+                if((nextChar = (short) FIn.read()) != -1){
+                    Ventana.append((char)nextChar);
+                    if(Ventana.length() > 65536) Ventana.deleteCharAt(0);
+                    FOut.write((char)nextChar);
+                }
+            }
+            else if(nextChar == 1){
+                int pos;
+                int tam;
+
+                if((nextInt = FIn.read()) != -1){
+                    pos = nextInt;
+                    if((nextChar = (short) FIn.read()) != -1){
+                        tam = nextChar;
+                        while(tam > 0){
+                            FOut.write(Ventana.charAt(pos));
+                            Ventana.append(Ventana.charAt(pos));
+                            ++pos;
+                            --tam;
+                        }
+                        while(Ventana.length() > 65536) Ventana.deleteCharAt(0);
+                    }
                 }
             }
         }
@@ -64,17 +160,5 @@ public class LZSS extends Algorithm{
         FOut.close();
     }
 
-    public void descomprimir(String pathfile, String) {
-
-        FileInputStream FIn = new FileInputStream(InFile);
-        FileOutputStream FOut = new FileOutputStream(NewPath + NewName + ".lzss");
-        bytebuffer Ventana = bytebuffer.allocate(4092);
-
-    }
-
-    public static void Main(String[] Args){
-
-        Comprimir(Args[1], Args[2], Args[3]);
-
-    }
 }
+
