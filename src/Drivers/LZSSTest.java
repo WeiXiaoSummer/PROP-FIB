eca8a3ef2b06205c297fcc1c1bfe1095d0171858
Pascual10 <56838611+Pascual10@users.noreply.github.com>
2019-11-16 17:36:41 +0100

    Add files via upload

--- src/Drivers/LZSSTest.java
+++ src/Drivers/LZSSTest.java
@@ -13,7 +13,7 @@ public class LZSSTest {
     private LZSS lzss = new LZSS(0, 0, 0, 0, 0, 0, 0);
 
     @Test
-    public void Comprimeix(){
+    public void ComprimeixPle(){
         String Path = new File("").getAbsolutePath() + "\\Prueba.txt";
         String text = DataCtrl.getInstance().getInputFile(Path);
         Fitxer fitxer = new Fitxer("", "", text);
@@ -24,7 +24,7 @@ public class LZSSTest {
     }
 
     @Test
-    public void Descomprimeix(){
+    public void DescomprimeixPle(){
         String Path = new File("").getAbsolutePath() + "\\Prueba.txt";
         String text = DataCtrl.getInstance().getInputFile(Path);
         Fitxer fitxer = new Fitxer("", "", text);
@@ -35,6 +35,27 @@ public class LZSSTest {
         assertTrue("L'algorisme LZSS no ha descomprimit correctament", (midaNormal == midaDescomprimida));
         String textDescomp = fitxerDescomp.getFileContent();
         assertFalse("L'algorisme LZSS no ha descomprimit correctament", (text == textDescomp));
+    }
+
+    @Test
+    public void ComprimeixBuit(){
+        String text = "";
+        Fitxer fitxer = new Fitxer("", "", text);
+        int midaNormal = text.length();
+        Fitxer fitxerComp = lzss.comprimir(fitxer);
+        int midaComprimida = fitxerComp.getFileContent().length();
+        assertTrue("L'algorisme LZSS no ha comprimit correctament", (midaNormal >= midaComprimida));
+    }
+
+    @Test
+    public void DescomprimeixBuit(){
+        String text = "";
+        Fitxer fitxer = new Fitxer("", "", text);
+        int midaNormal = text.length();
+        Fitxer fitxerComp = lzss.comprimir(fitxer);
+        Fitxer fitxerDescomp = lzss.descomprimir(fitxerComp);
+        int midaDescomprimida = fitxerDescomp.getFileContent().length();
+        assertTrue("L'algorisme LZSS no ha descomprimit correctament", (midaNormal == midaDescomprimida));
     }
 
 }
