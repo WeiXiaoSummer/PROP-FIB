fcf98a9e31dd8bfe5e5e06dec581a2c002f95076
Pascual10 <56838611+Pascual10@users.noreply.github.com>
2019-12-11 16:28:25 +0100

    Update JPEG.java

--- src/Domain/JPEG.java
+++ src/Domain/JPEG.java
@@ -10,11 +10,8 @@ import static java.lang.Math.*;
 
 public class JPEG extends Algorithm {
 
-    public JPEG(int numCompression, int numDecompression, int totalCompressedData,
-                int totalDecompressedData, double totalCompressionTime, double totalDecompressionTime,
-                double averageCompressionRatio) {
-        super(numCompression, numDecompression, totalCompressedData, totalDecompressedData, totalCompressionTime,
-                totalDecompressionTime, averageCompressionRatio);
+    public JPEG(GlobalStatistic estadistiques) {
+        super(estadistiques);
     }
 
     @Override
