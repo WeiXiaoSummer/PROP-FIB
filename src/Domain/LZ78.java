9ea6c391fd9a4679fe235d5bc684a8f4d8249596
sheng <sheng.liu@est.fib.upc.edu>
2019-12-18 10:29:54 +0100

    18/12/2019

--- src/Domain/LZ78.java
+++ src/Domain/LZ78.java
@@ -17,10 +17,13 @@ public class LZ78 extends Algorithm {
         long startTime=System.currentTimeMillis();
         byte[] text = file.getContent();
 
+        // compress
         TrieTree trieTree = new TrieTree();
         byte[] outStream = (trieTree.comprimir(text)).toByteArray();
 
-        long endTime=System.currentTimeMillis(); // get the time when end the compression
+        // get the time when end the compression
+        long endTime=System.currentTimeMillis();
+
         byte fileNameLength = (byte) file.getFile().getName().length();
         double compressTime = (double)(endTime-startTime)* 0.001;
         double compressRatio = text.length/outStream.length;
@@ -47,34 +50,36 @@ public class LZ78 extends Algorithm {
     public Pair<Double, Double> descomprimir(byte[] compressedContent, Fitxer file) throws IOException {
         long startTime=System.currentTimeMillis();
         byte[] text = compressedContent;
-
-        HashMap<Integer, byte[]> dic = new HashMap<>();
+        ArrayList<byte[]> dic = new ArrayList<>();
         ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        int key = 0;
+        int key;
         int i = 0;
-        boolean isInt = true;
+        //the structure of text is a int, a char, a int, a char...
         while (i < text.length) {
-            if (isInt) {
-                key = byteToInt(text, i);
-                i += 4;
-                if (key != 0) {
-                    byte[] value = dic.get(key);
-                    outStream.write(value);
-                    if (i < text.length){
-                        dic.put(dic.size()+1, addByte(value, text[i]));
-                    }
+            // now we read the int, it's 4 bytes
+            // transfer byte[] to int
+            key = byteToInt(text, i);
+            i += 4;
+            // it means that i'm reading some repeated words and I can get them in dic[key-1].
+            if (key != 0) {
+                byte[] value = dic.get(key-1);
+                // output the repeated words
+                outStream.write(value);
+                if (i < text.length){
+                    // add a byte into a byte[]
+                    // and insert it new byte[] into dic
+                    dic.add(addByte(value, text[i]));
                 }
-                else {
-                    byte[] in = new byte[1];
-                    in[0] = text[i];
-                    dic.put(dic.size()+1, in);
-                }isInt = false;
             }
-            else {
-                outStream.write(text[i]);
-                ++i;
-                isInt = true;
+            else { // no repested words
+                byte[] in = new byte[1];
+                in[0] = text[i];
+                // and insert new byte[] into dic
+                dic.add(in);
             }
+            // now we read the char, and output it.
+            if (i+1 < text.length)outStream.write(text[i]);
+            ++i;
         }
         long endTime=System.currentTimeMillis(); // get the time when end the compression
         double descompressTime = (double)(endTime-startTime)* 0.001;
@@ -86,12 +91,14 @@ public class LZ78 extends Algorithm {
         return new Pair<>(descompressTime, compressRatio);
     }
 
+    // transfer bytes to int
     public int byteToInt(byte[] bytes, int i) {
         return (bytes[i]&0xff)<<24
                 | (bytes[i+1]&0xff)<<16
                 | (bytes[i+2]&0xff)<<8
                 | (bytes[i+3]&0xff);
     }
+    // add a byte into a byte[]
     public byte[] addByte(byte[] bytes, byte b) {
         byte[] in = new byte[bytes.length + 1];
         for (int i = 0; i < bytes.length; ++i) {
--- src/Domain/TrieTree.java
+++ src/Domain/TrieTree.java
@@ -5,51 +5,55 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class TrieTree {
+    class Node{
+        public int element;
+        HashMap<Byte, Node> childdren = null;
 
-        class Node{
-            public int element;//单词的当前字
-            boolean wordEnd;//单词结束标志
-            HashMap<Byte, Node> childdren = null;
-
-            public Node(){
-                childdren = new HashMap<Byte, Node>();
-            }
-
-            public Node(int item){
-                this.element = item;
-                childdren = new HashMap<Byte, Node>();
-            }
+        public Node(){
+            childdren = new HashMap<Byte, Node>();
         }
 
-        public Node root;
-        public int key = 0;
-
-        //构造函数
-        public TrieTree(){
-            root = new Node();
-            root.element = 0;
+        public Node(int item){
+            this.element = item;
+            childdren = new HashMap<Byte, Node>();
         }
+    }
 
-        public ByteArrayOutputStream comprimir(byte[] input) throws IOException {
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            Node currentNode = root;
-            int i = 0;
-            while (i < input.length) {
-                Node childNode = currentNode.childdren.get(input[i]);
-                if (childNode == null) {
-                    Node child = new Node(++key);
-                    currentNode.childdren.put(input[i], child);
-                    output.write(intToByteArray(currentNode.element));
-                    if (i < input.length)output.write(input[i]);
-                    currentNode = root;
-                } else {
-                    currentNode = childNode;
-                    if (i+1 == input.length)output.write(intToByteArray(currentNode.element));
-                }
-                ++i;
+    private Node root;
+    private int key = 0;
+
+    public TrieTree(){
+        root = new Node();
+        root.element = 0;
+    }
+
+    public ByteArrayOutputStream comprimir(byte[] input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        // start at root of tree
+        Node currentNode = root;
+        int i = 0;
+        while (i < input.length) {
+            // check if currentNode has a child with char -> input[i]
+            Node childNode = currentNode.childdren.get(input[i]);
+            // if don't has
+            if (childNode == null) {
+                //create a child with char -> input[i]
+                Node child = new Node(++key);
+                currentNode.childdren.put(input[i], child);
+                output.write(intToByteArray(currentNode.element));
+                if (i < input.length)output.write(input[i]);
+                currentNode = root;
             }
-            return output;
+            // if has, continue check the childnode, until can't get the repeating byte
+            else {
+                currentNode = childNode;
+                // output the number of nodes we have check and it isn't null.
+                if (i+1 == input.length) output.write(intToByteArray(currentNode.element));
+            }
+            ++i;
         }
+        return output;
+    }
 
     public static byte[] intToByteArray(int value) {
         return new byte[] {
